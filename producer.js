04b2d05b46de16bbf7faccbf73ebc878159eb158
Chris Heinen <chris@heinencreative.com>
2015-07-19 19:14:19 -0400

    add calc to lib and create random expressions

--- producer.js
+++ producer.js
@@ -1,1 +1,65 @@
-//producer.js
+// Producer.js is designed to generate random math expression
+// and POST it as JSON {'expression':'1+2='} to the server
+var http = require('http');
+var calc = require('./lib/calc');
+var helper = require('./lib/helper');
+var argv = require('minimist')(process.argv.slice(2));
+
+var responseObj = {},
+    host = 'localhost',
+    port = argv.port || 3000, // Set port value, default 3000
+    integer = (typeof argv.integer !== 'undefined') ? (argv.integer === "true") : true, // Use integer or float in math expression, default = true
+    lowerLimit = argv.lowerLimit || 1, // Set lower limit of random numbers, default = 1
+    upperLimit = argv.upperLimit || 100, // Set upper limit of random numbers, default = 100
+    requestDelay = argv.requestDelay || 1000; // Time in ms between requests, default = 1000
+
+// POST a math expression at an interval of requestDelay
+var t = setInterval(function(){
+        var expression = '';
+        // Build a mathmatical expression from random numbers and operator
+        expression = calc.randomNumber(lowerLimit,upperLimit,integer);
+        expression += calc.operators[calc.randomNumber(0,3,true)];
+        expression += calc.randomNumber(lowerLimit,upperLimit,integer);
+        expression += '=';
+
+        helper.log('Generated: '+expression);
+        postExpression(expression);
+      },requestDelay);
+
+// Build and send a POST request containing json JSON. For example, {'expression','1+2='}
+function postExpression(expression) {
+  // JSON data to be sent
+  var postData = JSON.stringify({
+    'expression' : expression
+  });
+
+  // HTTP request options
+  var options = {
+    hostname: host,
+    port: port,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length
+    }
+  };
+
+  // Build requst
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    // Log response data
+    res.on('data', function (chunk) {
+      helper.log('Response: '+chunk);
+    });
+  });
+
+  // Log errors
+  req.on('error', function(e) {
+    helper.log('Problem with request: ' + e.message);
+  });
+
+  // Write JSON data to request body
+  req.write(postData);
+  req.end();
+}
--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ var chai = require("chai"),
     sinonChai = require('sinon-chai'),
     sinon = require('sinon');
 
-var helper = require('../lib/helper');
+var helper = require('../lib/helper'),
+    calc = require('../lib/calc');
 
 chai.use(sinonChai);
 
@@ -23,6 +24,31 @@ describe('Helper',function(){
       var regexTest = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)+\ \- .*/.test(console.log.getCall(0).args[0]);
       expect(console.log).to.have.been.calledOnce;
       expect(regexTest).to.be.true;
+    });
+  });
+});
+
+describe('Calc',function(){
+  describe('#operators', function(){
+    it('should contain +,-,/,*', function () {
+      expect(calc.operators).to.include('+');
+      expect(calc.operators).to.include('-');
+      expect(calc.operators).to.include('/');
+      expect(calc.operators).to.include('*');
+    });
+  });
+  describe('#randomNumber()',function(){
+    it('should return an integer', function () {
+      var result = calc.randomNumber(1,10,true);
+      assert.isNumber(result);
+      var isInt = result % 1 === 0;
+      assert(isInt, 'not an integer: ' + result);
+    });
+    it('should return a float', function () {
+      var result = calc.randomNumber(1,10,false);
+      assert.isNumber(result);
+      var isInt = result % 1 !== 0;
+      assert(isInt, 'not a float: ' + result);
     });
   });
 });
