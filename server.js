427797e82b1b80e06d5e97c77a477477c09517df
Chris Heinen <chris@heinencreative.com>
2015-07-19 18:52:43 -0400

    add basic server and log test

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-//server.js
+// Server.js is a HTTP server designed to receive a math
+// expressions in JSON, compute the answer and respond
+// with either the correct answer or an appropriate error.
+var http = require('http');
+var url = require('url');
+var helper = require('./lib/helper');
+var argv = require('minimist')(process.argv.slice(2));
+
+var port = argv.port || 3000; // Set port value, default 3000
+
+// Handle math expression requests
+function requestHandler(req,res) {
+    var body = '',
+        requestObj = {},
+        result = {};
+
+    // Write data to body
+    req.on('data', function(data) {
+        body += data.toString();
+    });
+
+    // End request and parse request body as JSON
+    req.on('end', function() {
+        if (body) requestObj = JSON.parse(body);
+        helper.log('Request: '+body);
+        res.end(JSON.stringify({status:"success"}));
+    });
+}
+
+// Fire it up!
+http.createServer(requestHandler).listen(port,function(){
+    console.log('Listening for requests on port: ' + port);
+});
--- test/test.js
+++ test/test.js
@@ -1,1 +1,28 @@
-// test.js
+var chai = require("chai"),
+    expect = chai.expect,
+    assert = chai.assert,
+    sinonChai = require('sinon-chai'),
+    sinon = require('sinon');
+
+var helper = require('../lib/helper');
+
+chai.use(sinonChai);
+
+describe('Helper',function(){
+  describe('#log()',function(){
+    beforeEach(function() {
+      sinon.spy(console,'log');
+    });
+    afterEach(function() {
+      console.log.restore();
+    });
+    it('should log timestamp and message', function () {
+      var message = "Mocha test";
+      helper.log(message);
+      // datetime regex - http://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+      var regexTest = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)+\ \- .*/.test(console.log.getCall(0).args[0]);
+      expect(console.log).to.have.been.calledOnce;
+      expect(regexTest).to.be.true;
+    });
+  });
+});
