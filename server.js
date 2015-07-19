867db2074eae7e30e2ab4392f8a53228738ed80b
Chris Heinen <chris@heinencreative.com>
2015-07-19 19:25:51 -0400

    add parse method and test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var http = require('http');
 var url = require('url');
 var helper = require('./lib/helper');
+var calc = require('./lib/calc');
 var argv = require('minimist')(process.argv.slice(2));
 
 var port = argv.port || 3000; // Set port value, default 3000
@@ -23,6 +24,31 @@ function requestHandler(req,res) {
     req.on('end', function() {
         if (body) requestObj = JSON.parse(body);
         helper.log('Request: '+body);
+        // If no expression provided, respond with error
+        if (!requestObj.hasOwnProperty('expression')) {
+            result.status = 'error';
+            result.message = "Expecting a POST request with JSON. For example, {'expression':'1+2='}";
+            helper.log('Response: '+JSON.stringify(result));
+            res.statusCode = 400;
+            res.end(JSON.stringify(result));
+            return;
+        }
+
+        // Parse the request JSON and return an array
+        var parsed = helper.parse(requestObj.expression);
+
+        // If expression could not be parsed
+        if (!parsed) {
+            result.status = 'fail';
+            result.data = {};
+            result.data.expression = "Invalid expression, '"+requestObj.expression+"'. Should follow pattern, '1+2='.";
+            helper.log('Response: '+JSON.stringify(result));
+            res.statusCode = 400;
+            res.end(JSON.stringify(result));
+            return;
+        }
+
+        // TODO this is a temporary response, replace with actual calculation
         res.end(JSON.stringify({status:"success"}));
     });
 }
--- test/test.js
+++ test/test.js
@@ -26,6 +26,36 @@ describe('Helper',function(){
       expect(regexTest).to.be.true;
     });
   });
+  describe('#parse()',function(){
+    it('should return an array with a length of 3',function(){
+      expect(helper.parse('1+2=')).to.be.an('Array');
+      expect(helper.parse('1+2=')).to.have.length(3);
+    });
+    it('should return an integer for first element in array',function(){
+      expect(helper.parse('1+2=')[0]).to.equal('1');
+    });
+    it('should return a float for first element in array',function(){
+      expect(helper.parse('1.001+2=')[0]).to.equal('1.001');
+    });
+    it('should return null if first element in expression is not a number',function(){
+      expect(helper.parse('Q+2=')).to.be.null;
+    });
+    it('should return an operator for second element in array',function(){
+      expect(helper.parse('1+2=')[1]).to.equal('+');
+    });
+    it('should return null if second element in expression is not an operator',function(){
+      expect(helper.parse('1;2=')).to.be.null;
+    });
+    it('should return an integer for third element in array',function(){
+      expect(helper.parse('1+2=')[2]).to.equal('2');
+    });
+    it('should return a float for third element in array',function(){
+      expect(helper.parse('1+2.001=')[2]).to.equal('2.001');
+    });
+    it('should return null if third element in expression is not a number',function(){
+      expect(helper.parse('1+P=')).to.be.null;
+    });
+  });
 });
 
 describe('Calc',function(){
